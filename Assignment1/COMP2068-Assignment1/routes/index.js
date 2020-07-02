ef7d737ed1e20da89d2a854e57d5ba62a3de0af8
Michael VM <michaelvaladaomartins@gmail.com>
2020-07-01 23:07:26 -0400

    changed my anchor tags in header and footer to pug anchor tags. add little bit of css and added information for home page and about me page

--- Assignment1/COMP2068-Assignment1/routes/index.js
+++ Assignment1/COMP2068-Assignment1/routes/index.js
@@ -4,23 +4,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Michael Valadao-Martins Personal Portfolio' });
 });
 
 router.get('/about', function (req, res) {
-    res.render('about', { title: 'Express' });
+    res.render('about', { title: 'About Me' });
 });
 
 router.get('/projects', function (req, res) {
-    res.render('projects', { title: 'Express' });
+    res.render('projects', { title: 'My Projects' });
 });
 
 router.get('/services', function (req, res) {
-    res.render('services', { title: 'Express' });
+    res.render('services', { title: 'My Services' });
 });
 
 router.get('/contact', function (req, res) {
-    res.render('contact', { title: 'Express' });
+    res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
